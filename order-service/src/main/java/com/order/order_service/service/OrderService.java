eca81bbb581ed1f386d0e8967c7ecc23f4cfa9b9
Meghana-VS <meghavs7022@gmail.com>
2025-08-16 13:19:16 +0530

    Spring Cloud OpenFeign & Wiremock

--- order-service/src/main/java/com/order/order_service/service/OrderService.java
+++ order-service/src/main/java/com/order/order_service/service/OrderService.java
@@ -1,11 +1,12 @@
 package com.order.order_service.service;
 
+import com.order.order_service.client.InventoryClient;
 import com.order.order_service.dto.OrderRequest;
 import com.order.order_service.model.Order;
 import com.order.order_service.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.UUID;
 
@@ -15,13 +16,23 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+    @Autowired
+    private InventoryClient inventoryClient;
 
-        orderRepository.save(order);
+    public void placeOrder(OrderRequest orderRequest){
+
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRepository.save(order);
+        }else {
+            throw new RuntimeException("Product with skuCode "+orderRequest.skuCode()+" is not in stock");
+        }
     }
 }
--- order-service/src/test/java/com/order/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/order/order_service/OrderServiceApplicationTests.java
@@ -1,19 +1,32 @@
 package com.order.order_service;
 
+import com.order.order_service.model.Order;
+import com.order.order_service.repository.OrderRepository;
+import com.order.order_service.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.when;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@LocalServerPort
 	private Integer port;
+
+	@MockitoBean
+	private OrderRepository mockOrderRepository;
 
 	@BeforeEach
 	void setup(){
@@ -23,9 +36,19 @@ class OrderServiceApplicationTests {
 
 	@Test
 	void shouldPlaceOrder() {
+
+		Order savedOrder = new Order();
+		savedOrder.setSkuCode("iphone_15");
+		savedOrder.setPrice(new BigDecimal("1200"));
+		savedOrder.setQuantity(1);
+
+		when(mockOrderRepository.save(any(Order.class))).thenReturn(savedOrder);
+
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
+
 		String submitOrderJson = """
 				{
-				    "skuCode": "IPhone 15",
+				    "skuCode": "iphone_15",
 				    "price": "1200",
 				    "quantity": 1
 				}
